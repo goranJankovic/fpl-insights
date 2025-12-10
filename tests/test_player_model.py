8ff2d4fc42c84a1556f961e072caad400fcc8b69
Jankovic <goran.jankovic@4zida.rs>
2025-12-10 22:58:23 +0100

    update readme

--- tests/test_player_model.py
+++ tests/test_player_model.py
@@ -1,8 +1,8 @@
 from models.player_model import predict_player_points
 from models.monte_carlo import MonteCarlo
 
-player_id = 430 #Haaland
-gw = 14
+player_id = 414 #Foden
+gw = 16
 
 mean, std = predict_player_points(player_id, gw)
 mc = MonteCarlo(n_sims=10000)
--- tests/test_team_basic.py
+++ tests/test_team_basic.py
@@ -2,7 +2,7 @@ from predictions.team_basic import predict_team_points
 
 # test with any 11 players
 sample_team = [366, 8, 261, 407, 16, 119, 237, 414, 283, 249, 430]
-gw = 14
+gw = 16
 
 dist = predict_team_points(sample_team, gw)
 print(dist.summary())
